e34998159b1f68bc3cfe80a33265af3f9e41f622
dbhinton <dhinton.us@gmail.com>
2021-12-15 15:15:36 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -30,23 +30,18 @@ app.use('/api/users', userRoutes);
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   )
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....')
-//   })
-// }
-// console.log(__dirname)
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+console.log(__dirname)
 
 app.use(notFound)
 app.use(errorHandler)
